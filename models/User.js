6854bc8f89b93f8914fe56ee06652f1cc30d469d
Carmezim <carmezim.filho@gmail.com>
2017-05-01 02:15:54 +0100

    adding model validation on username and emal

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
+  username: {type: String, lowercase: true, requred: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, ' is invalid'], index: true},
+  email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\s+@\S+\.\S+/, 'is invalid'], index: true},
   bio: String,
   image: String,
   hash: String,
