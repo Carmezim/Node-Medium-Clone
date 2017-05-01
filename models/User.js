ab2508f2d8e0ffa5b431a051e0c045f00704a269
Carmezim <carmezim.filho@gmail.com>
2017-05-01 03:24:27 +0100

    password validation

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const crypto = require('crypto');
 
-// schema setup
+
+// Schema setup
 const UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, ' is invalid'], index: true},
   email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\s+@\S+\.\S+/, 'is invalid'], index: true},
@@ -12,13 +13,20 @@ const UserSchema = new mongoose.Schema({
   salt: String
 }, {timestamps: true});
 
-// unique validation
-UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 // Hashes and salt
-UserSchema.methods.setPassword = password => {
+UserSchema.methods.setPassword = (password) => {
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
+
+// Password validation
+UserSchema.methods.validPassword = (password) => {
+  let hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 mongoose.model('user', UserSchema);
