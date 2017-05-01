82fbef2b96048c49f0f9a602744f945834b9b8e8
Carmezim <carmezim.filho@gmail.com>
2017-05-01 03:02:29 +0100

    adding hash and salt

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
 
+// schema setup
 const UserSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, ' is invalid'], index: true},
   email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\s+@\S+\.\S+/, 'is invalid'], index: true},
@@ -10,6 +12,13 @@ const UserSchema = new mongoose.Schema({
   salt: String
 }, {timestamps: true});
 
+// unique validation
 UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+
+// Hashes and salt
+UserSchema.methods.setPassword = password => {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
 
 mongoose.model('user', UserSchema);
