e437ce74aecb6a442a414fc025ade67d30c4cab3
Carmezim <carmezim.filho@gmail.com>
2017-06-01 03:33:31 +0200

    authentication routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
-var router = require('express').Router();
+const router = require('express').Router();
 
-router.use('/api', require('./api'));
+router.use('/', require('./users'));
 
+router.use((err, req, res, next) => { 
+  if (err.name === 'validationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.erros).reduce((errors, key) => {
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {})
+    });
+  }
+
+  return next(err);
+});
 module.exports = router;
